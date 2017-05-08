9d770f58dabf08d49d830986bd7279ef69d3467d
Emmanuel Brun <Emmanuel Brun@DESKTOP-JIQNSRV.form-domain.lan>
2017-05-08 19:43:57 +0200

    Modif dans businness utiles pour ficheobjetBean Fiche objet bean en cours Ajout de string redirection dans connexionBean pour changer chemin redirection après connexion Modif dao participant requete rechercherproprio(objet objet)

--- GREENWISH_POM/GREENWISH_WEB/src/main/java/fr/afcepf/ai100/g3/FicheObjetBean.java
+++ GREENWISH_POM/GREENWISH_WEB/src/main/java/fr/afcepf/ai100/g3/FicheObjetBean.java
@@ -1,26 +1,93 @@
 package fr.afcepf.ai100.g3;
 
-import javax.ejb.EJB;
-import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
+import java.util.List;
 
-@javax.faces.bean.ManagedBean(name="mbFicheObjet")
+import javax.ejb.EJB;
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name="mbFicheObjet")
 @SessionScoped
 public class FicheObjetBean {
-	
+
 	@EJB
 	private IBusinessCatalogue proxyCatalogue;
-	
+
+	@EJB
+	private IBusinessFicheObjet proxyFicheObjet;
+
 	private Objet objet;
-	
-	
+	private List<Image> images;
+	private String valeur;
+	public ConnexionBean getCnxBean() {
+		return cnxBean;
+	}
+
+	public void setCnxBean(ConnexionBean cnxBean) {
+		this.cnxBean = cnxBean;
+	}
+
+	private String idAge;
+	private boolean isConnecte;
+	private boolean NotMyObjet = true;
+	private boolean ConnecteAndNotMyObjet;
+
+
+	@ManagedProperty(value="#{mbCnx}")
+	private ConnexionBean cnxBean;
+
 	public String afficherObjet(Objet objet){
 		this.objet = objet;
-		System.out.println(objet.getIntitule());
+		this.images = proxyFicheObjet.getImageByIdObjet(objet.getIdobjet());
+		this.valeur = objet.getValeur().toString();
+		this.setIdAge(objet.getTrancheAge().getIdage().toString());
 		return "/FicheObjet.xhtml?faces-redirect=true";
 	}
-	
-	
+
+	public boolean estVide(String str){
+		boolean estVide;
+		if(str.isEmpty()){
+			estVide=true;
+		}
+		else{
+			estVide=false;
+		}
+		return estVide;
+
+	}
+
+	public void estConnecte() {
+		if(cnxBean.getParticipant() == null) {
+			ConfigurableNavigationHandler  nav =
+					(ConfigurableNavigationHandler)
+					FacesContext.getCurrentInstance()
+					.getApplication()
+					.getNavigationHandler();
+			isConnecte = true;
+			cnxBean.setPageRedirection("/FicheObjet.xhtml?faces-redirect=true");
+			nav.performNavigation("/Connexion.xhtml?faces-redirect=true&amp;callback=focus");
+		}else{
+			if(cnxBean.getParticipant().getIdparticipant() == proxyFicheObjet.recupProprio(objet.getIdobjet()).getIdparticipant()){
+				ConnecteAndNotMyObjet=false;
+				NotMyObjet=false;
+			}
+			else{
+				ConnecteAndNotMyObjet=true;
+				NotMyObjet=true;
+			}
+			ConfigurableNavigationHandler  nav =
+					(ConfigurableNavigationHandler)
+					FacesContext.getCurrentInstance()
+					.getApplication()
+					.getNavigationHandler();
+			nav.performNavigation("/FicheObjet.xhtml?faces-redirect=true");
+
+		}
+	}
+
 	public IBusinessCatalogue getProxyCatalogue() {
 		return proxyCatalogue;
 	}
@@ -28,9 +95,6 @@ public class FicheObjetBean {
 	public void setProxyCatalogue(IBusinessCatalogue proxyCatalogue) {
 		this.proxyCatalogue = proxyCatalogue;
 	}
-
-
-
 
 	public Objet getObjet() {
 		return objet;
@@ -40,8 +104,77 @@ public class FicheObjetBean {
 		this.objet = objet;
 	}
 
-	
-	
-	
-	
+
+	public IBusinessFicheObjet getProxyFicheObjet() {
+		return proxyFicheObjet;
+	}
+
+
+	public void setProxyFicheObjet(IBusinessFicheObjet proxyFicheObjet) {
+		this.proxyFicheObjet = proxyFicheObjet;
+	}
+
+
+	public List<Image> getImages() {
+		return images;
+	}
+
+
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+
+
+	public String getValeur() {
+		return valeur;
+	}
+
+
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+
+
+	public String getIdAge() {
+		return idAge;
+	}
+
+
+	public void setIdAge(String idAge) {
+		this.idAge = idAge;
+	}
+
+
+
+	public boolean isConnecte() {
+		return isConnecte;
+	}
+
+	public void setConnecte(boolean isConnecte) {
+		this.isConnecte = isConnecte;
+	}
+
+	public boolean isNotMyObjet() {
+		return NotMyObjet;
+	}
+
+	public void setNotMyObjet(boolean notMyObjet) {
+		NotMyObjet = notMyObjet;
+	}
+
+	public boolean isConnecteAndNotMyObjet() {
+		return ConnecteAndNotMyObjet;
+	}
+
+	public void setConnecteAndNotMyObjet(boolean connecteAndNotMyObjet) {
+		ConnecteAndNotMyObjet = connecteAndNotMyObjet;
+	}
+
+
+
+
+
+
+
+
 }
