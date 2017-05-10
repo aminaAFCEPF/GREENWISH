bc6c8c8602040a725e61c38aa26e105abc83084c
Stagiaire <Stagiaire@AFCEPF-8N5CF3S2>
2017-05-10 14:05:04 +0200

    dispo presque fini

---
+++ GREENWISH_POM/GREENWISH_BUSINESS_API/src/main/java/fr/afcepf/ai100/g3/IBusinessGestionDispo.java
@@ -0,0 +1,8 @@
+package fr.afcepf.ai100.g3;
+
+
+
+public interface IBusinessGestionDispo {
+
+	void ajouterDispo();
+}
--- GREENWISH_POM/GREENWISH_DATA_API/src/main/java/fr/afcepf/ai100/g3/IDaoDisponibilite.java
+++ GREENWISH_POM/GREENWISH_DATA_API/src/main/java/fr/afcepf/ai100/g3/IDaoDisponibilite.java
@@ -3,4 +3,7 @@ package fr.afcepf.ai100.g3;
 public interface IDaoDisponibilite {
 	Disponibilite getDisponibiliteByIdParticipant (int id);
 	Disponibilite getDisponibiliteById (int id);
+	Disponibilite ajouterDisponibilite(Disponibilite disponibilite);
+	Disponibilite updateDisponibilite(Disponibilite disponibilite);
+	void deleteDisponibilite(Disponibilite disponibilite);
 }
